2eea17bf605b187aa9fcab5328bad7e3959706c9
ashwinib935 <110533153+ashwinib935@users.noreply.github.com>
2022-08-17 15:53:06 +0530

    docs: Added some details of application

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Do-You-Know_Me
+It is a CLI quiz app for Ashwini's friends and families to check how well do they know her 😁. This super interesting quiz is made using nodejs.
+
+# Work Flow
+Firstly, It takes username from user to know who's attending the quiz .
+
+After that it asked few questions to user related to Ashwini to check how well do they know her.
+
+on the basis of the answers given , the user will get the scores accordingly . Please note that , there's no negative points for wrong / incorrect answers .
+
+If you beat the high score then it will show Congratulation message and ask to send screenshot.
+
+Show the highest score details .
+
+# Packages used:
+readline-sync
+chalk
+
  
---
+++ index.js
@@ -0,0 +1,92 @@
+var readlineSync = require('readline-sync');
+var chalk = require('chalk');
+
+const welcomemsg = chalk.blue.bold;
+const right = chalk.bgGreenBright.black;
+const wrong = chalk.bgRedBright.black;
+const congratsMessage = chalk.yellow.bold;
+
+var userScore = 0;
+
+let questions = [
+
+  questionOne = {
+    question: "What is Ashwini's nickname?",
+    answer: "Komal"
+  },
+  questionTwo = {
+    question: "Who is Ashwini's favorite hero?",
+    answer: "Sidhart Manotra"
+  },
+  questionThree = {
+    question: "Which is Ashwini's favorite song?",
+    answer: "Kal ho na ho"
+  },
+  questionFour = {
+    question: "Which is Ashwini's favorite color?",
+    answer: "Pink"
+  },
+  questionFive = {
+    question: "Which is Ashwini's hobby?",
+    answer: "Traveling"
+  }
+]
+let highScore = [{ name: "Shamal", score: 4 }, { name: "Shantanu", score: 3 }];
+
+
+
+
+
+function welcome() {
+  var userName = readlineSync.question("What's your name? ");
+  console.log(welcomemsg("Welcome " + userName + " to DO YOU KNOW Ashwini?"));
+
+}
+
+function play(question, answer) {
+  let userAnswer = readlineSync.question(question);
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log(right("You are right"));
+    userScore++;
+  }
+  else {
+    console.log(wrong("You are wrong"));
+  }
+  console.log("Current score:", userScore);
+  console.log("----------------");
+}
+
+function game() {
+  for (let i = 0; i < questions.length; i++) {
+    play(questions[i].question, questions[i].answer);
+  }
+}
+
+function showScore() {
+  if (userScore > 0) {
+    console.log("YAY! You SCORED: ", userScore);
+  }
+  else {
+    console.log("You SCORED: ", userScore, "Please try again...");
+  }
+  console.log("**********************")
+  console.log("Highest Score Details")
+  for (let i = 0; i < highScore.length; i++) {
+    console.log("Name:", highScore[i].name + "   " + "Score: " + highScore[i].score);
+  }
+
+}
+
+function calculateHighestScore() {
+  console.log("**********************");
+  for (let i = 0; i < highScore.length; i++) {
+    if (userScore > highScore[i].score)
+      return console.log(congratsMessage("Congratulations you beat the score.Please send the screenshot so I can update it on my database"));
+
+  }
+}
+
+welcome();
+game();
+showScore();
+calculateHighestScore();
